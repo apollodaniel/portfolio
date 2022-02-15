c462980b21a3f16ce61ca47011db5d0b326fcb91
apollo <developer.apollo.mail@gmail.com>
2022-02-15 07:11:13 -0300

    Added on click in project container

--- lib/responsive/Components/Desktop/DesktopProjectContainer.dart
+++ lib/responsive/Components/Desktop/DesktopProjectContainer.dart
@@ -2,6 +2,7 @@ import 'package:awesome_icons/awesome_icons.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:portfolio/Home.dart';
+import 'dart:js' as js;
 
 class DesktopProjectContainer extends StatefulWidget {
   int index;
@@ -21,74 +22,79 @@ class _DesktopProjectContainerState extends State<DesktopProjectContainer> {
     if (repositories[index].license != null) {
       _license = repositories[index].license!.name ?? "Other";
     }
-    return Container(
-      padding: EdgeInsets.all(32),
-      decoration: BoxDecoration(
-        color: Color(0xFF1e0040),
-        borderRadius: BorderRadius.circular(12),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Spacer(
-            flex: 2,
-          ),
-          Text(
-            repositories[index].name.length > 15
-                ? "${repositories[index].name.substring(0, 15)}..."
-                : repositories[index].name,
-            style: TextStyle(fontSize: 24, fontWeight: FontWeight.w900),
-          ),
-          SizedBox(
-            height: 16,
-          ),
-          Text(
-              repositories[index].description.length > 48
-                  ? "${repositories[index].description.substring(0, 48)}..."
-                  : repositories[index].description,
-              style: TextStyle(fontSize: 16, fontWeight: FontWeight.w300)),
-          Spacer(),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Icon(FontAwesomeIcons.paperclip),
-                  SizedBox(
-                    width: 8,
-                  ),
-                  Text(_license)
-                ],
-              ),
-              Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Icon(Icons.star),
-                  SizedBox(
-                    width: 8,
-                  ),
-                  Text(repositories[index].stargazersCount.toString())
-                ],
-              ),
-              Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Icon(Icons.storage),
-                  SizedBox(
-                    width: 8,
-                  ),
-                  Text(
-                      "${nf.format(repositories[index].size / 1000).toString()} MB")
-                ],
-              ),
-            ],
-          ),
-          Spacer(
-            flex: 2,
-          )
-        ],
+    return GestureDetector(
+      onTap: () {
+        js.context.callMethod("open", [repositories[index].htmlUrl]);
+      },
+      child: Container(
+        padding: EdgeInsets.all(32),
+        decoration: BoxDecoration(
+          color: Color(0xFF1e0040),
+          borderRadius: BorderRadius.circular(12),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Spacer(
+              flex: 2,
+            ),
+            Text(
+              repositories[index].name.length > 15
+                  ? "${repositories[index].name.substring(0, 15)}..."
+                  : repositories[index].name,
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.w900),
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            Text(
+                repositories[index].description.length > 48
+                    ? "${repositories[index].description.substring(0, 48)}..."
+                    : repositories[index].description,
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.w300)),
+            Spacer(),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Icon(FontAwesomeIcons.paperclip),
+                    SizedBox(
+                      width: 8,
+                    ),
+                    Text(_license)
+                  ],
+                ),
+                Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Icon(Icons.star),
+                    SizedBox(
+                      width: 8,
+                    ),
+                    Text(repositories[index].stargazersCount.toString())
+                  ],
+                ),
+                Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Icon(Icons.storage),
+                    SizedBox(
+                      width: 8,
+                    ),
+                    Text(
+                        "${nf.format(repositories[index].size / 1000).toString()} MB")
+                  ],
+                ),
+              ],
+            ),
+            Spacer(
+              flex: 2,
+            )
+          ],
+        ),
       ),
     );
   }
