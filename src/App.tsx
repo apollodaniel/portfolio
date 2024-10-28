866ce9500e072d446923f0a64cfcf374dc818442
Apollo Daniel <developer.apollo.mail@gmail.com>
2024-10-28 11:44:26 -0300

    added navbar, and created repository cache on session storage

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,30 @@ import {
 } from 'react';
 import AboutMeSection from './components/about_me';
 import PresentationSection from './components/presentation';
-import { Button } from '@nextui-org/react';
-import { Outlet } from 'react-router-dom';
+import {
+	Button,
+	Navbar,
+	NavbarContent,
+	NavbarItem,
+	NavbarMenuToggle,
+	Link,
+	NavbarMenu,
+	NavbarMenuItem,
+	Dropdown,
+	DropdownTrigger,
+	DropdownMenu,
+	DropdownItem,
+	Accordion,
+	AccordionItem,
+} from '@nextui-org/react';
+import { Outlet, useLocation, useNavigate } from 'react-router-dom';
 import ProjectsSection from './components/projects';
 import { Repository } from './types';
 import { get_repositories } from './functions';
 import ContactSection from './components/contact';
+
+import { faAngleDown as ArrowIcon } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export const RepositoriesContext = createContext<Repository[] | undefined>([]);
 
@@ -59,6 +77,10 @@ function App() {
 			const repository_list = await get_repositories();
 
 			setRepositories(repository_list);
+			window.sessionStorage.setItem(
+				'repositories',
+				JSON.stringify(repository_list),
+			);
 		} catch (err) {
 			setRepositories(undefined);
 		}
@@ -73,14 +95,45 @@ function App() {
 		});
 		navigateScroll.current(window.location.pathname);
 
-		getRepositories.current();
+		const cached_repos = window.sessionStorage.getItem('repositories');
+		if (!cached_repos) {
+			getRepositories.current();
+		} else {
+			setRepositories(JSON.parse(cached_repos));
+		}
 	}, []);
 
 	useEffect(() => {
 		if (window.location.pathname === '/projects') {
 			projectsSectionRef.current?.scrollIntoView({ behavior: 'smooth' });
 		}
+		console.log(repositories);
 	}, [repositories]);
+
+	const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+	const menuItems = [
+		{ name: 'Home', path: '/' },
+		{ name: 'About me', path: '/about-me' },
+		{
+			name: 'Projects',
+			path: '/projects',
+			children: [
+				{
+					name: 'All projects',
+					path: '/projects/all',
+				},
+			],
+		},
+		{ name: 'Contact', path: '/contact' },
+	];
+
+	const navigate = useNavigate();
+	const location = useLocation();
+
+	useEffect(() => {
+		navigateScroll.current(location.pathname);
+	}, [location]);
 
 	return (
 		<RepositoriesContext.Provider value={repositories}>
@@ -89,7 +142,160 @@ function App() {
 
 
 				*/}
+			<Navbar isMenuOpen={isMenuOpen} onMenuOpenChange={setIsMenuOpen}>
+				<NavbarContent>
+					<NavbarMenuToggle
+						aria-label={isMenuOpen ? 'Close menu' : 'Open menu'}
+						className="sm:hidden"
+					/>
+				</NavbarContent>
 
+				<NavbarContent
+					className="hidden sm:flex gap-4"
+					justify="center"
+				>
+					{menuItems.map((item) =>
+						item.children && item.children.length > 0 ? (
+							<Dropdown>
+								<NavbarItem>
+									<DropdownTrigger className="cursor-pointer">
+										<Link
+											className="cursor-pointer"
+											color={
+												location.pathname.includes(
+													item.path,
+												)
+													? 'primary'
+													: 'foreground'
+											}
+										>
+											{location.pathname.includes(
+												'/projects',
+											)
+												? item.children.find(
+														(child) =>
+															child.path ===
+															location.pathname,
+													)?.name || 'Projects'
+												: 'Projects'}
+											<FontAwesomeIcon
+												className="ms-1"
+												icon={ArrowIcon}
+											/>
+										</Link>
+									</DropdownTrigger>
+								</NavbarItem>
+								<DropdownMenu>
+									{[item, ...item.children].map((child) => (
+										<DropdownItem>
+											<Link
+												className="cursor-pointer"
+												color={
+													location.pathname ===
+													child.path
+														? 'primary'
+														: 'foreground'
+												}
+												onClick={() => {
+													navigate(child.path);
+												}}
+											>
+												{child.name}
+											</Link>
+										</DropdownItem>
+									))}
+								</DropdownMenu>
+							</Dropdown>
+						) : (
+							<NavbarItem>
+								<Link
+									className="cursor-pointer"
+									color={
+										location.pathname === item.path
+											? 'primary'
+											: 'foreground'
+									}
+									onClick={() => {
+										navigate(item.path);
+									}}
+								>
+									{item.name}
+								</Link>
+							</NavbarItem>
+						),
+					)}
+				</NavbarContent>
+				<NavbarMenu>
+					{menuItems.map((item, index) =>
+						item.children && item.children.length > 0 ? (
+							<Accordion className="m-0 p-0 " isCompact>
+								<AccordionItem
+									className="flex flex-col items-start justify-content-start m-0 p-0"
+									title={
+										<Link
+											className="w-full cursor-pointer pointer-events-none"
+											size="lg"
+											color={
+												location.pathname.includes(
+													item.path,
+												)
+													? 'primary'
+													: 'foreground'
+											}
+										>
+											{location.pathname.includes(
+												'/projects',
+											)
+												? item.children.find(
+														(child) =>
+															child.path ===
+															location.pathname,
+													)?.name || 'Projects'
+												: 'Projects'}
+										</Link>
+									}
+								>
+									{[item, ...item.children].map((child) => (
+										<Link
+											className="w-full cursor-pointer"
+											onClick={() => {
+												navigate(child.path);
+												setIsMenuOpen(false);
+											}}
+											size="lg"
+											color={
+												location.pathname === child.path
+													? 'primary'
+													: 'foreground'
+											}
+										>
+											{child.name}
+										</Link>
+									))}
+								</AccordionItem>
+							</Accordion>
+						) : (
+							<NavbarMenuItem key={`${item.name}-${index}`}>
+								<Link
+									className="w-full cursor-pointer"
+									onClick={() => {
+										navigate(item.path);
+										setIsMenuOpen(false);
+									}}
+									size="lg"
+									color={
+										location.pathname === item.path
+											? 'primary'
+											: 'foreground'
+									}
+								>
+									{item.name}
+								</Link>
+							</NavbarMenuItem>
+						),
+					)}
+				</NavbarMenu>
+			</Navbar>
 			{location.pathname === '/projects/all' ? (
 				<Outlet />
 			) : (
